6759c77d167d06b5ec425e0b6c71f2adb8b2d81e
daniel-ryan-PM <43017203+daniel-ryan-PM@users.noreply.github.com>
2018-10-22 16:48:28 -0700

    Update docker-entrypoint.sh
    point sed commands to php71 conf files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,19 @@
 set -ex
 
 # ProcessMaker required configurations
-sed -i '/short_open_tag = Off/c\short_open_tag = On' /etc/php.ini
-sed -i '/post_max_size = 8M/c\post_max_size = 24M' /etc/php.ini
-sed -i '/upload_max_filesize = 2M/c\upload_max_filesize = 24M' /etc/php.ini
-sed -i '/;date.timezone =/c\date.timezone = America/New_York' /etc/php.ini
-sed -i '/expose_php = On/c\expose_php = Off' /etc/php.ini
+sed -i '/short_open_tag = Off/c\short_open_tag = On' /etc/php-7.1.ini
+sed -i '/post_max_size = 8M/c\post_max_size = 24M' /etc/php-7.1.ini
+sed -i '/upload_max_filesize = 2M/c\upload_max_filesize = 24M' /etc/php-7.1.ini
+sed -i '/;date.timezone =/c\date.timezone = America/New_York' /etc/php-7.1.ini
+sed -i '/expose_php = On/c\expose_php = Off' /etc/php-7.1.ini
 
 # OpCache configurations
-sed -i '/;opcache.enable_cli=0/c\opcache.enable_cli=1' /etc/php.d/10-opcache.ini
-sed -i '/opcache.max_accelerated_files=4000/c\opcache.max_accelerated_files=10000' /etc/php.d/10-opcache.ini
-sed -i '/;opcache.max_wasted_percentage=5/c\opcache.max_wasted_percentage=5' /etc/php.d/10-opcache.ini
-sed -i '/;opcache.use_cwd=1/c\opcache.use_cwd=1' /etc/php.d/10-opcache.ini
-sed -i '/;opcache.validate_timestamps=1/c\opcache.validate_timestamps=1' /etc/php.d/10-opcache.ini
-sed -i '/;opcache.fast_shutdown=0/c\opcache.fast_shutdown=1' /etc/php.d/10-opcache.ini
+sed -i '/;opcache.enable_cli=0/c\opcache.enable_cli=1' /etc/php-7.1.d/10-opcache.ini
+sed -i '/opcache.max_accelerated_files=4000/c\opcache.max_accelerated_files=10000' /etc/php-7.1.d/10-opcache.ini
+sed -i '/;opcache.max_wasted_percentage=5/c\opcache.max_wasted_percentage=5' /etc/php-7.1.d/10-opcache.ini
+sed -i '/;opcache.use_cwd=1/c\opcache.use_cwd=1' /etc/php-7.1.d/10-opcache.ini
+sed -i '/;opcache.validate_timestamps=1/c\opcache.validate_timestamps=1' /etc/php-7.1.d/10-opcache.ini
+sed -i '/;opcache.fast_shutdown=0/c\opcache.fast_shutdown=1' /etc/php-7.1.d/10-opcache.ini
 
 # Decompress ProcessMaker
 cd /tmp && tar -C /opt -xzvf processmaker-3.3.0.tar.gz
